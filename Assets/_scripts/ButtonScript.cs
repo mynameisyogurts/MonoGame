9deeefc4f33a37718e7e7e9dead891b73c009b06
Matthew Freeberg (RIT Student) <mjf6999@ad.rit.edu>
2016-12-03 16:54:44 -0500

    New Intro scene with button

---
+++ Assets/_scripts/ButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    void OnClick ()
+    {
+        Debug.Log("Button has been pressed");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    
+	}
+}
